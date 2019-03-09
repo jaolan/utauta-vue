12e23b9538f52e59dc9115f112d8138ae8fc6ca6
Jorel Olan <superman@Jorels-MBP.lan1>
2019-03-08 23:10:17 -0600

    added styling, navbar, searchbar components

--- utauta-js/src/App.vue
+++ utauta-js/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Utauta"/>
+    <nav-bar/>
+    <a href=#><img style="padding-top: 30px;" src="https://img.discogs.com/AkOrci0VGW4EyocudygYkGQC0iI=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4777778-1465593637-3821.png.jpg" height="300" width="300"></a>
+    <SearchBar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from './components/NavBar.vue'
+import SearchBar from './components/SearchBar.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar,
+    SearchBar
   }
 }
 </script>
@@ -23,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ utauta-js/src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <h2>Get your groove on.</h2>
+        <input
+            @input="onInput"
+            class="search-bar"
+            placeholder="Search"
+        />
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'SearchBar'
+}
+</script>
+
+<style scoped>
+
+    .search-bar{
+        text-align: left;
+        width: 75%;
+        padding: 10px 20px;
+        border-radius: 12px;
+        border: 1px solid rgb(179, 179, 179);
+        margin-top: 20px;
+    }
+
+    .header{
+        text-align: center;
+        margin: 20px;
+        margin-top: 50px;
+        color: aliceblue
+
+    }
+</style>
